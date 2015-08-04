cd0ca302963d5726e7a8d385cd27bae6260d6634
eric <chunchet@gmail.com>
2015-08-04 10:37:04 +0800

    getting_and_cleaning_data commit

---
+++ getting_and_cleaning_data/project/README.md
@@ -0,0 +1,16 @@
+# Getting and Cleaning Data
+Project of the Coursera course in data science, **Getting and Cleaning Data**. 
+As part of an understanding in generating a tidy dataset from the raw wearable
+computing data collected from the accelerometers from the Samsung Galaxy S 
+smartphone.
+
+## The Project
+To create a R script called run_analysis.R that does the following:
+1. Merges the training and the test sets to create one data set.
+2. Extracts only the measurements on the mean and standard deviation for each 
+   measurement. 
+3. Uses descriptive activity names to name the activities in the data set
+4. Appropriately labels the data set with descriptive variable names. 
+5. From the data set in step 4, creates a second, independent tidy data set 
+   with the average of each variable for each activity and each subject.
+
---
+++ getting_and_cleaning_data/project/run_analysis.R
@@ -0,0 +1,56 @@
+## Create one R script called run_analysis.R that does the following:
+## 1. Merges the training and the test sets to create one data set.
+## 2. Extracts only the measurements on the mean and standard deviation 
+##    for each measurement.
+## 3. Uses descriptive activity names to name the activities in the data set
+## 4. Appropriately labels the data set with descriptive activity names.
+## 5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+
+## Pre-requisition
+# check for required packages
+install.required.package <- function(pkg) {
+	if(!pkg %in% rownames(installed.packages())) {
+		install.packages(need, dependencies = TRUE, quietly = T)
+	}
+}
+
+packages <- c("data.table", "reshape2")
+lapply(packages, install.required.package)
+
+
+## 1. Merges the training and the test sets to create one data set
+# read data
+train.label	<- read.table("UCI\ HAR\ Dataset/train/y_train.txt")
+train.subject	<- read.table("UCI\ HAR\ Dataset/train/subject_train.txt")
+train.data	<- read.table("UCI\ HAR\ Dataset/train/X_train.txt")
+test.label	<- read.table("UCI\ HAR\ Dataset/test/y_test.txt")
+test.subject	<- read.table("UCI\ HAR\ Dataset/test/subject_test.txt")
+test.data	<- read.table("UCI\ HAR\ Dataset/test/X_test.txt")
+
+# bind the training & test data in the format: label, subject, data
+data <- rbind(train.data, test.data)
+
+## 2. Extracts only the measurements on the mean and standard deviation 
+##    for each measurement. 
+feature.label <- read.table("UCI\ HAR\ Dataset/features.txt", 
+			stringsAsFactors=F)
+idx.meanstd <- grep("mean|std", feature.label[, 2])
+data <- data[, idx.meanstd]
+
+## 3. Uses descriptive activity names to name the activities in the data set
+activity.label <- read.table("UCI\ HAR\ Dataset/activity_labels.txt")
+data.label <- rbind(train.label, test.label)
+data.label$labelName = activity.label[data.label[, 1], 2]
+
+## 4. Appropriately labels the data set with descriptive variable names.
+data <- cbind(data.label, rbind(train.subject, test.subject), data)
+label.id <- c("label", "labelName", "subject")
+names(data) <- c(label.id, feature.label[idx.meanstd, 2])
+
+## 5. Creates a second, independent tidy data set with 
+##    the average of each variable for each activity and each subject.
+melt.data <- melt(data, id=label.id, measure.vars=feature.label[idx.meanstd, 2])
+tidy.data <- dcast(melt.data, labelName + subject ~ variable, mean)
+
+write.table(tidy.data, file = "./UCI\ HAR\ Dataset/tidy_data.txt")
